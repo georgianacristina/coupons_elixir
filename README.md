103fb4e28f21a5940f58f8975cae7e6e57219332
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-04-21 21:47:51 +0300

    Add server

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # coupons_elixir
+
+# Install
+
+Install dependecies:
+	mix deps.get
+
+Compile:
+
+	iex -S mix
+
+When prompt 'iex(1)>' appears, write:
+	Plug.Adapters.Cowboy.http(Coupons, [])
+
+Server should be available at http://0.0.0.0:4000/
--- coupons/lib/coupons.ex
+++ coupons/lib/coupons.ex
@@ -1,18 +1,12 @@
 defmodule Coupons do
-  @moduledoc """
-  Documentation for Coupons.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Coupons.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def init(default_options) do
+     IO.puts "initializing plug"
+     default_options   
+  end
+  
+  def call(conn, options) do
+     IO.puts "calling plug"
+     conn
+     |> Plug.Conn.send_resp(200, "hello world")
   end
 end
